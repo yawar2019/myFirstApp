f4f2b9d4bc35cba07c52ea91774dc66479cf5947
Yawar2019 <yawarali17@gmail.com>
2025-08-13 08:28:33 +0530

    commit initial code

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,12 +1,81 @@
 import { Component, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Contact } from './contact/contact';
+import { FormsModule } from '@angular/forms';
+
+
+interface IContact
+{
+ id:number;
+ name:string;
+ email:string; 
+}
 
 @Component({
-  selector: 'app-root',
-  imports: [RouterOutlet],
-  templateUrl: './app.html',
+  selector: '[app-root]',
+  imports: [RouterOutlet,Contact,FormsModule],
+  templateUrl:'./app.html',
   styleUrl: './app.css'
 })
+
+
 export class App {
-  protected readonly title = signal('myFirstApp');
+  title:string="hi everyone";
+  hero="shaktiman";
+  srcUrl="https://www.searchenginejournal.com/wp-content/uploads/2019/04/the-seo-guide-to-angular.png";
+
+firstsize="firstsizecss";
+firstbackcolorcss="firstbackcolorcss";
+
+firstCssArr=["first","firstsizecss","firstbackcolorcss"];
+firstCssObj=
+{
+first:true,
+firstsizecss:true,
+firstbackcolorcss:true
+}
+
+hastrue=true;
+firstStyle={"color":"blue","background-color":"red"};
+
+secondStyle=this.hastrue?"red":"green";
+
+  getResult(a:any)
+  {
+alert(a+" is a value");
+  }
+
+getEventValue(event:any)
+  {
+    console.log(event);
+  }
+
+  UserName="test";
+
+  cars=['BMW','NANO','AUDI'];
+  sports=[
+          {id:1,name:'cricket'}
+          ,{id:2,name:'hockey'},
+          {id:3,name:'kabadi'}
+        ]
+
+        contacts:IContact[]=[
+          {
+          id:1,
+          name:'ravi',
+          email:'ravi@123gmail.com',
+        },
+
+        {
+          id:2,
+          name:'swati',
+          email:'swati@123gmail.com'
+        },
+        {
+          id:3,
+          name:'prakash',
+          email:'prakash@123gmail.com'
+        },
+        
+        ]
 }
--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,8 @@
 import { bootstrapApplication } from '@angular/platform-browser';
 import { appConfig } from './app/app.config';
 import { App } from './app/app';
+import { About } from './app/about/about';
+import { Contact } from './app/contact/contact';
 
 bootstrapApplication(App, appConfig)
   .catch((err) => console.error(err));
