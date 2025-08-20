9babf5a2eb6dc261e7d38a9f1408b9991821f245
Yawar2019 <yawarali17@gmail.com>
2025-08-20 09:00:31 +0530

    Reading values from url

--- src/app/about/about.ts
+++ src/app/about/about.ts
@@ -1,4 +1,5 @@
-import { Component, EventEmitter, Input, Output } from '@angular/core';
+import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
+import { ActivatedRoute, Params } from '@angular/router';
 
 @Component({
   selector: 'app-about',
@@ -6,14 +7,54 @@ import { Component, EventEmitter, Input, Output } from '@angular/core';
   templateUrl: './about.html',
   styleUrl: './about.css'
 })
-export class About {
+export class About implements OnInit {
 FirstName="Raj shekhar";
 @Input() parentdata:string='';
 @Output() childata=new EventEmitter<string>();
+
+user={
+  id:"",
+  name:""
+}
+
+
+constructor(private router:ActivatedRoute)
+{
+
+}
+  ngOnInit(): void {
+  // this.user={
+  //   id:this.router.snapshot.params['id'],
+  //   name:this.router.snapshot.params['name']
+  // }
+this.router.params.subscribe((data:Params)=>{
+  this.user={
+    id:data['id'],
+    name:data['name'],
+  }
+})
+// console.log(this.router.snapshot.queryParams);
+// console.log(this.router.snapshot.fragment);
+
+this.router.queryParams.subscribe((data)=>{console.log(data)});
+
+
+this.router.fragment.subscribe((data)=>{console.log(data)});
+
+  }
+
 
 ChildToParent()
 {
   this.childata.emit('Good Morning');
 }
+
+
+
+
+
+
+
+
 
 }
