2ac424a1c33a70646fcfe9e8503aa7cb2266756f
Yawar2019 <yawarali17@gmail.com>
2026-01-05 09:04:51 +0530

    behavioural Example

--- src/app/rxjx-example/rxjx-example.ts
+++ src/app/rxjx-example/rxjx-example.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
-import { catchError, concatMap, delay, filter, interval, map, mergeMap, Observable, of, take, throwError,Subject } from 'rxjs';
+import { catchError, concatMap, delay, filter, interval, map, mergeMap, Observable, of, take, throwError,Subject, BehaviorSubject } from 'rxjs';
+import { ajax } from 'rxjs/ajax';
 
 @Component({
   selector: 'app-rxjx-example',
@@ -19,6 +20,8 @@ export class RxjxExample {
 //this.OperatorExampleConcatMap();
 //this.OperatorExampleMergeMap();
 //this.SubscribeExample();
+//this.SubjectExampl1();
+this.BehaviouralSubjectExample();
 }
 fetchData()
 {
@@ -117,9 +120,84 @@ result.subscribe(x=>console.log(x));
 }
 
 
+SubjectExampl1()
+{
+  //begining observable example
+  // let obs=new Observable((obs)=>{obs.next(Math.random() )});
+
+  // //subscribe1
+  // obs.subscribe((data)=>console.log('Subscriber 1:'+data));
+
+  // //subscribe2
+  // obs.subscribe((data)=>console.log('Subscriber 2:'+data));
+  //Ending Of Observable Example
+
+
+
+  //Begining of Subject Example
+
+  // const subject = new Subject();
+  // subject.subscribe((data) => console.log('Subject Subscriber 1:' + data));
+  // subject.subscribe((data) => console.log('Subject Subscriber 2:' + data));
+  // subject.next(Math.random());
+
+  //Ending of Subject Example
+
+
+  //Begining of Subject Example Consumer
+
+  
+  // const data=ajax('https://jsonplaceholder.typicode.com/users');
+  // data.subscribe((res)=>console.log('Subject Subscriber 1:',res));
+  // data.subscribe((res)=>console.log('Subject Subscriber 2:',res));
+  // data.subscribe((res)=>console.log('Subject Subscriber 3:',res));
+ 
+
+
+
+//   const subject = new Subject();
+//   const data=ajax('https://jsonplaceholder.typicode.com/users');
+//   subject.subscribe((res)=>console.log('Subject Subscriber 1:',res));
+//   subject.subscribe((res)=>console.log('Subject Subscriber 2:',res));
+//   subject.subscribe((res)=>console.log('Subject Subscriber 3:',res));
+//   data.subscribe(subject);//data is producer and subject is consumer
+}
+
+
+SubjectExample3()
+{
+
+  const subject = new  Subject();
+  //subscribe1
+  subject.subscribe((data)=>console.log('Subscriber 1:'+data));
+
+  //subscribe2
+  subject.subscribe((data)=>console.log('Subscriber 2:'+data));
+  subject.next(200);
+  //subscribe3
+  subject.subscribe((data)=>console.log('Subscriber 3:'+data));
+  subject.next(300);
+}
+
+
+// BehaviouralSubjectExample()
+// {
+
+//   const subject = new BehaviorSubject(100);
+//   //subscribe1
+//   subject.subscribe((data)=>console.log('Subscriber 1:'+data));
+
+//   //subscribe2
+//   subject.subscribe((data)=>console.log('Subscriber 2:'+data));
+//   subject.next(200);
+//   //subscribe3
+//   subject.subscribe((data)=>console.log('Subscriber 3:'+data));
+//   subject.next(300);
+// }
+
 SubscribeExample()
 {
-  const subject = new Subject<number>();
+  const subject = new Subject();
  
 subject.subscribe({
   next: (v) => console.log(`observerA: ${v}`),
